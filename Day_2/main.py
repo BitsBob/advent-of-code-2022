538e44a025bc309670f79536a4f7890416b65887
Audey <Audey@DaddyAds-iMac.lan>
2022-12-05 19:13:38 +0000

    commenting changes, begin part 2

--- Day_2/main.py
+++ Day_2/main.py
@@ -58,10 +58,48 @@ for int in totals:
 print(final)
 
 # Part 2
-# A scores 1
-# B scores 2
-# C scores 3
+# A scores 1 ROCK
+# B scores 2 PAPER
+# C scores 3 SCISSORS
 # Y = Draw scores 3
 # Z = Win scores 6
 # X = Loss scores 0
 
+totals = []
+score = 0
+
+for pair in pairs:
+     score = 0
+     if pair[2] == "X":
+          score += 1
+          if pair[0] == "A":
+               score += 3
+               totals.append(score)
+          if pair[0] == "B":
+               score += 0
+               totals.append(score)
+          if pair[0] == "C":
+               score += 6
+               totals.append(score)
+     if pair[2] == "Y":
+          score += 2
+          if pair[0] == "A":
+               score += 6
+               totals.append(score)
+          if pair[0] == "B":
+               score += 3
+               totals.append(score)
+          if pair[0] == "C":
+               score += 0
+               totals.append(score)
+     if pair[2] == "Z":
+          score += 3
+          if pair[0] == "A":
+               score += 0
+               totals.append(score)
+          if pair[0] == "B":
+               score += 6
+               totals.append(score)
+          if pair[0] == "C":
+               score += 3
+               totals.append(score)
