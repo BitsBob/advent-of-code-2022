1e7f62b4fbc0f3ca111d8061666cfe44fef372cc
Audey <audeygodfrey@gmail.com>
2022-12-06 18:55:33 +0000

    finished d3p2

--- Day_3/main.py
+++ Day_3/main.py
@@ -18,3 +18,15 @@ for entry in data:
 
 print("Answer to part 1: ", totalSum)
 
+totalSum = 0
+j = 3
+
+for i in range(0, len(data), 3):
+    entry = data[i:j]
+    j += 3
+    
+    for value, character in enumerate(ascii_letters):
+        if character in entry[0] and character in entry[1] and character in entry[2]:
+            totalSum += value + 1
+
+print("Answer to part 2: ", totalSum)
