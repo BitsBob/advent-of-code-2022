78b7296040501b8c04bb815b64164a3d6d234f2f
Audey <audeygodfrey@gmail.com>
2022-12-06 18:57:02 +0000

    added day four and changed file extensions

--- Day_3/main.py
+++ Day_3/main.py
@@ -1,7 +1,7 @@
 from string import ascii_letters
 
 # Getting data
-with open('Day_3/input_data.in') as file:
+with open('Day_3/input_data.txt') as file:
     data = [i for i in file.read().strip().split("\n")]
 
 totalSum = 0
@@ -24,7 +24,7 @@ j = 3
 for i in range(0, len(data), 3):
     entry = data[i:j]
     j += 3
-    
+
     for value, character in enumerate(ascii_letters):
         if character in entry[0] and character in entry[1] and character in entry[2]:
             totalSum += value + 1
