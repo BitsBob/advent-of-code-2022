8d3f550e6ba58de741531641d081334e4a5cb5e4
Audey <audeygodfrey@gmail.com>
2022-12-07 18:28:16 +0000

    finished d4 start d5

--- Day_4/main.py
+++ Day_4/main.py
@@ -5,5 +5,13 @@ for line in open('Day_4/input_data.txt', 'r'):
      two = [i for i in range(int(b.split('-')[0]), int(b.split('-')[1])+1)]
      if all(item in one for item in two) or all(item in two for item in one):
           total += 1
+print(total)
 
+total = 0
+for line in open('Day_4/input_data.txt', 'r'):
+     a, b = line.split(",")
+     one = [i for i in range(int(a.split('-')[0]), int(a.split('-')[1])+1)]
+     two = [i for i in range(int(b.split('-')[0]), int(b.split('-')[1])+1)]
+     if any(item in one for item in two) or any(item in two for item in one):
+          total += 1
 print(total)
