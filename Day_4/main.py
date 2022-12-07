73bd70fd0200cdab8fd87a49fff77d603af99a38
Audey <audeygodfrey@gmail.com>
2022-12-07 17:39:57 +0000

    renamed variable

--- Day_4/main.py
+++ Day_4/main.py
@@ -1,9 +1,9 @@
-t = 0
+total = 0
 for line in open('Day_4/input_data.txt', 'r'):
      a, b = line.split(",")
      one = [i for i in range(int(a.split('-')[0]), int(a.split('-')[1])+1)]
      two = [i for i in range(int(b.split('-')[0]), int(b.split('-')[1])+1)]
      if all(item in one for item in two) or all(item in two for item in one):
-          t += 1
+          total += 1
 
-print(t)
+print(total)
