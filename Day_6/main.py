b43ae821f79dba01bbf70aaa4176529fd09cdaff
Audey <audeygodfrey@gmail.com>
2022-12-07 19:41:02 +0000

    added whitespace to day  6

--- Day_6/main.py
+++ Day_6/main.py
@@ -2,6 +2,7 @@ def findMarkerPartOne(data):
 	for i in range(0, (len(data) + 3)):
 		if len(set(data[i:i+4])) == 4:
 			return i + 4, data[i:i+4]
+
 def findMarkerPartTwo(data):
 	for i in range(0, (len(data) + 13)):
 		if len(set(data[i:i+14])) == 14:
